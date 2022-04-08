3d876169127827a2394acd70c0824c6907e5b237
Aleco112 <aleco112@yahoo.fr>
2022-04-08 04:01:12 +0000

    css

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,58 +4,61 @@ const Home = () => {
 	// state for input
 	const [listname, setListName] = useState("");
 	// state for array
-	const [todolist, setTodoList] = useState([]);
+	const [todolist, setTodoList] = useState(["hello", "now", "stop"]);
 
 	return (
 		<>
-			<div className="input-group">
-				<input
-					type="text"
-					className="form-control"
-					placeholder="Todo-list"
-					onChange={(event) => {
-						setListName(event.target.value);
-					}}
-					value={listname}
-					onKeyUp={(e) => {
-						if (e.key == "Enter" && listname !== "") {
-							setTodoList([...todolist, listname]);
-							setListName("");
-						}
-					}}
-				/>
-				<button
-					onClick={() => {
-						//check is input is empty 
-						if (listname !== "") {
-							setTodoList([...todolist, listname]);
-							setListName("");
-						}
-					}}
-					className="btn btn-outline-secondary"
-					type="button">
-					Add task
-				</button>
-			</div>
-			<ul>
-				{todolist.map((item, index) => {
-					return (
-						<li key={index}>
-							{item}
-							<button
-								onClick={() =>
-									setTodoList(
-										todolist.filter((item, i) => {
-											return index !== i;
-										})
-									)
-								}>
-								X
-							</button>
-						</li>
-					);
-				})}
-			</ul>
+			<main>
+				<h1> Todos</h1>
+				<div className="input-group">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Todo-list"
+						onChange={(event) => {
+							setListName(event.target.value);
+						}}
+						value={listname}
+						onKeyUp={(e) => {
+							if (e.key == "Enter" && listname !== "") {
+								setTodoList([...todolist, listname]);
+								setListName("");
+							}
+						}}
+					/>
+					<button
+						onClick={() => {
+							//check is input is empty
+							if (listname !== "") {
+								setTodoList([...todolist, listname]);
+								setListName("");
+							}
+						}}
+						className="btn btn-outline-secondary"
+						type="button">
+						Add task
+					</button>
+				</div>
+				<ul>
+					{todolist.map((item, index) => {
+						return (
+							<li className="newtodos" key={index}>
+								{item}
+								<button
+									onClick={() =>
+										setTodoList(
+											todolist.filter((item, i) => {
+												return index !== i;
+											})
+										)
+									}>
+									X
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+			</main>
 		</>
 	);
 };
